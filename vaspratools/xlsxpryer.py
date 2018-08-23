fab6f7474c36365b81ad109548fce22ff37c3fb0
Vaspra <douglawrence0010@gmail.com>
2018-08-23 15:41:41 +0100

    Added 'fill_col_with_data_dict'

--- vaspratools/xlsxpryer.py
+++ vaspratools/xlsxpryer.py
@@ -44,3 +44,97 @@ def format_list_to_commastring(ls):
     string = string.strip(',')
     
     return string
+
+
+def fill_col_with_data_dict(ws, key_col, insert_col, data_dict,
+                       exact_match=False, insert_col_name=''):
+    """
+    For each entry in 'data_dict', this function will look for the entry's
+    key in all the cells of the column 'key_col'. If 'in' one of these cells
+    (or '==' if given 'exact_match=True'), the entry's value will be inserted
+    into the row's 'insert_col' cell.
+    
+    Forces a lower case comparison (tEsT == test == TEST), if not using
+    'exact_match=True'
+    
+    If an 'insert_col_name' argument is provided, the column header will be
+    set to this value.
+    """
+    
+    assert key_col.isalpha,\
+        '\'key_col\' (%s) argument must be alphabetic' % key_col
+        
+    assert insert_col.isalpha,\
+        '\'insert_col\' (%s) argument must be alphabetic' % insert_col
+    
+    empties = 0
+    inserts = 0
+    unmatched = 0
+    
+    for row in range(1, len(ws[key_col]) + 1):           
+        r = str(row)
+        success = False 
+        for key, value in data_dict.items():
+            
+            if not ws[key_col + r].value:
+                print('Empty cell [%s]: %s'\
+                      % (key_col + r, ws[key_col + r].value))
+                empties += 1
+                break
+            
+            if not exact_match:
+                if key.lower().strip() in ws[key_col + r].value\
+                                          .lower().strip():
+                    ws[insert_col + r].value = value
+                    inserts += 1
+                    success = True
+                    break
+            else:
+                if key == ws[key_col + r].value:
+                    ws[insert_col + r].value = value
+                    inserts += 1
+                    success = True
+                    break
+        
+        if ws[key_col + r].value and not success:          
+            print('Unmatched cell [%s]: %s'\
+                  % (key_col + r, ws[key_col + r].value))
+            unmatched += 1
+                
+    if insert_col_name:
+        ws[insert_col + '1'].value = insert_col_name
+                
+    print('\nInserted %d / %d data entries from dictionary into column: \'%s\''\
+          % (inserts, len(ws[key_col]), insert_col))
+    print('%d empty cells, %d unmatched cells'\
+          % (empties, unmatched))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                
+                
+                
+                
+                
+                
+                
+                
